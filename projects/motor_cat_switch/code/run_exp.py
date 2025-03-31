637453f69ba0217dda13b6d535bf240198a78056
MISS Saakshi Maneyalil <46602496@students.mq.edu.au>
2025-03-31 18:25:56 +1100

    Added experiment data for <your_name>

--- projects/motor_cat_switch/code/run_exp.py
+++ projects/motor_cat_switch/code/run_exp.py
@@ -4,7 +4,7 @@ from util_func import *
 if __name__ == "__main__":
 
     # set subject number
-    subject = 1001
+    subject = 1002
     dir_data = "../data"
     f_name = f"sub_{subject}_data.csv"
     full_path = os.path.join(dir_data, f_name)
